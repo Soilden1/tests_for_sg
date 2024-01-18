5be5f6623279dcdd5f1c74409f082b62c0552b5e
Dmitry_Smirnov <javatest9k6k5@gmail.com>
2024-01-18 21:39:28 +0300

    fix

--- src/test/java/tests/AuthorizationTests.java
+++ src/test/java/tests/AuthorizationTests.java
@@ -3,7 +3,6 @@ package tests;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Owner;
 import io.qameta.allure.Severity;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
